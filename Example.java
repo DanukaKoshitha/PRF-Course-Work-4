0f938613b8eb93df66b0b3cc6b23f002b3d1dd79
Danuka koshitha <danukakoshitha2004@gmail.com>
2024-11-28 20:31:16 +0530

    first commit

---
+++ Example.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Example{
+	public static void main(String args[]){	
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("                                                 \r\n                                          \r\n \r\n                      __   ______             __           \r\n                     |  \\ /      \\           |  \\          \r\n                      \\$$|  $$$$$$\\  ______  | $$  _______ \r\n                     |  \\| $$   \\$$ |      \\ | $$ /       \\\r\n                     | $$| $$        \\$$$$$$\\| $$|  $$$$$$$\r\n                     | $$| $$   __  /      $$| $$| $$      \r\n                     | $$| $$__/  \\|  $$$$$$$| $$| $$_____ \r\n                     | $$ \\$$    $$ \\$$    $$| $$ \\$$     \\\r\n                      \\$$  \\$$$$$$   \\$$$$$$$ \\$$  \\$$$$$$$\r\n                                      \r                                      \r                                     \r");
+		System.out.println("                                                 \r\n                                          \r\n  _   _                 _                  _____                          _            \r\n | \\ | |               | |                / ____|                        | |           \r\n |  \\| |_   _ _ __ ___ | |__   ___ _ __  | |     ___  _ ____   _____ _ __| |_ ___ _ __ \r\n | . ` | | | | \'_ ` _ \\| \'_ \\ / _ \\ \'__| | |    / _ \\| \'_ \\ \\ / / _ \\ \'__| __/ _ \\ \'__|\r\n | |\\  | |_| | | | | | | |_) |  __/ |    | |___| (_) | | | \\ V /  __/ |  | ||  __/ |   \r\n |_| \\_|\\__,_|_| |_| |_|_.__/ \\___|_|     \\_____\\___/|_| |_|\\_/ \\___|_|   \\__\\___|_|   \r\n                                                                                       \r                                                                                       \r");
+		System.out.println("---------------------------------------------------------------------------------------");
+		System.out.println("\n");
+		
+		System.out.println("	[01] Decimal Converter");
+		System.out.println("	[02] Binary Converter");
+		System.out.println("	[03] Octal Converter");
+		System.out.println("	[04] Hexadecimal Converter");
+		System.out.println("	[05] Riman Number Converter");
+		System.out.println("\n");
+
+		System.out.print("Enter Option -> ");
+		int option=input.nextInt();
+		
+		switch(option){
+			case 1 :
+			System.out.println("+_______________________________________________________+");
+			System.out.println("|       	     Decimal Converter                  |");
+			System.out.println("+-------------------------------------------------------+");
+			System.out.println("\n");
+
+		System.out.print("Enter an Decimal number : ");
+		int decimal1=input.nextInt();
+		System.out.println("\n");
+
+		/*String binary1="";
+		while (decimal1!=0)
+		{
+			binary1+=decimal1%2;
+			decimal1/=2;
+		}
+		System.out.println("	Binary number : "+binary1);*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+	}
+}
